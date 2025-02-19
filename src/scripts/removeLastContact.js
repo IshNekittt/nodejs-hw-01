712bd198ff0d94537720e118f054883b6037444b
Nekittt <nikitaishchenko334@gmail.com>
2025-02-19 16:07:35 +0200

    Implement removing of the last contact

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,3 +1,18 @@
-export const removeLastContact = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+export const removeLastContact = async () => {
+  try {
+    const currentMass = await readContacts();
+    if (currentMass.length > 0) {
+      currentMass.length = currentMass.length - 1;
+      await writeContacts(currentMass);
+    } else {
+      throw new Error('There are no contacts left');
+    }
+  } catch (err) {
+    console.log(err.message);
+  }
+};
 
 removeLastContact();
