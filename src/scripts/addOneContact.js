7f15c5b7257b0e2b150727417f5a6d08f10654b9
Nekittt <nikitaishchenko334@gmail.com>
2025-02-19 15:42:20 +0200

    Add functional of addOneContact

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,3 +1,15 @@
-export const addOneContact = async () => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+export const addOneContact = async () => {
+  try {
+    const savedContactsMass = await readContacts();
+    const newMass = [...savedContactsMass, createFakeContact()];
+    await writeContacts(newMass);
+  } catch (err) {
+    console.log(err.message);
+  }
+};
 
 addOneContact();
