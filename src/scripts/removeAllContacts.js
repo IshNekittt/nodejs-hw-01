0d97f7a73e90a2380d327d6d084af61e7e43e4c8
Nekittt <nikitaishchenko334@gmail.com>
2025-02-19 15:58:52 +0200

    Implement removeAllContacts function

--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -1,3 +1,15 @@
-export const removeAllContacts = async () => {};
+import { writeContacts } from '../utils/writeContacts.js';
+
+export const removeAllContacts = async () => {
+  await writeContacts([]);
+};
+
+// one more way to implement this
+// import { readContacts } from '../utils/readContacts.js';
+// export const removeAllContacts = async () => {
+//   const currentMass = await readContacts();
+//   currentMass.length = 0;
+//   await writeContacts(currentMass);
+// };
 
 removeAllContacts();
